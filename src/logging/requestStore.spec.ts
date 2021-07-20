4c2d2d8b4bc84bb2fcd66373c15416515803e15a
Kuo <kuo.cheng.wu@gmail.com>
2021-07-20 09:17:41 +0800

    fail

--- src/logging/requestStore.spec.ts
+++ src/logging/requestStore.spec.ts
@@ -2,7 +2,7 @@ import { get, getStore, set } from './requestStore';
 
 describe('Request store', () => {
   it('should fail', () => {
-    expect(1).toBe(1);
+    expect(1).toBe(12);
   })
 
   it('should return saved item from the store', () => {
